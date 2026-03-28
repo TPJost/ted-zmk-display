593959f2bdf4bb3554efa3719461806e91d83fb1
TPJost <111567316+TPJost@users.noreply.github.com>
2026-03-28 21:56:29 +0100

    Refactor screen object creation in status screen

--- src/custom_status_screen.c
+++ src/custom_status_screen.c
@@ -8,6 +8,7 @@
 #include "widgets/status.h"
 
 #include <zephyr/logging/log.h>
+
 LOG_MODULE_DECLARE(zmk, CONFIG_ZMK_LOG_LEVEL);
 
 #if IS_ENABLED(CONFIG_NICE_VIEW_WIDGET_STATUS)
@@ -15,8 +16,7 @@ static struct zmk_widget_status status_widget;
 #endif
 
 lv_obj_t *zmk_display_status_screen(void) {
-    lv_obj_t *screen;
-    screen = lv_obj_create(NULL);
+    lv_obj_t *screen = lv_obj_create(NULL);
 
 #if IS_ENABLED(CONFIG_NICE_VIEW_WIDGET_STATUS)
     zmk_widget_status_init(&status_widget, screen);
