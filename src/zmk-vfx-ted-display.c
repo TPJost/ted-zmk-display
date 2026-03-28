bf10cf057320bc5b927cc11d2216b436cf5ed8a0
TPJost <111567316+TPJost@users.noreply.github.com>
2026-03-28 20:16:25 +0100

    Update comments and include directive in display module

--- src/zmk-vfx-ted-display.c
+++ src/zmk-vfx-ted-display.c
@@ -2,17 +2,17 @@
  * Ted ZMK display module
  */
 
-#include <zephyr/kernel.h>
+#include <zephyr/init.h>
 #include <zephyr/logging/log.h>
 
 LOG_MODULE_REGISTER(zmk_vfx_ted_display, LOG_LEVEL_INF);
 
-/* Init function to verify module is loaded */
+/* Init function to verify module integration */
 static int zmk_vfx_ted_display_init(void)
 {
     LOG_INF("Ted display module loaded");
     return 0;
 }
 
-/* Run after system init */
+/* Register module init during application startup */
 SYS_INIT(zmk_vfx_ted_display_init, APPLICATION, 90);
