2e21b1175894f38681535b363cf534b87390bf87
TPJost <111567316+TPJost@users.noreply.github.com>
2026-03-28 21:44:32 +0100

    Implement status screen creation in custom_status_screen
    Refactor Ted ZMK display module to create a status screen.

--- src/custom_status_screen.c
+++ src/custom_status_screen.c
@@ -1,18 +1,27 @@
 /*
- * Ted ZMK display module
+ *
+ * Copyright (c) 2023 The ZMK Contributors
+ * SPDX-License-Identifier: MIT
+ *
  */
 
-#include <zephyr/init.h>
+#include "widgets/status.h"
+
 #include <zephyr/logging/log.h>
+LOG_MODULE_DECLARE(zmk, CONFIG_ZMK_LOG_LEVEL);
 
-LOG_MODULE_REGISTER(zmk_vfx_ted_display, LOG_LEVEL_INF);
+#if IS_ENABLED(CONFIG_NICE_VIEW_WIDGET_STATUS)
+static struct zmk_widget_status status_widget;
+#endif
 
-/* Init function to verify module integration */
-static int zmk_vfx_ted_display_init(void)
-{
-    LOG_INF("Ted display module loaded");
-    return 0;
+lv_obj_t *zmk_display_status_screen(void) {
+    lv_obj_t *screen;
+    screen = lv_obj_create(NULL);
+
+#if IS_ENABLED(CONFIG_NICE_VIEW_WIDGET_STATUS)
+    zmk_widget_status_init(&status_widget, screen);
+    lv_obj_align(zmk_widget_status_obj(&status_widget), LV_ALIGN_TOP_LEFT, 0, 0);
+#endif
+
+    return screen;
 }
-
-/* Register module init during application startup */
-SYS_INIT(zmk_vfx_ted_display_init, APPLICATION, 90);
